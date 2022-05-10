b7b8682eead913acfec9ce995e496b6dfbd70078
Jorge Puerta <jorge.puerta00@gmail.com>
2022-05-10 07:33:13 -0500

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { DrawDTO } from './common/dto/draw.dto';
 import { TicketDTO } from './common/dto/ticket.dto';
+import { UserPickDTO } from './common/dto/userpick.dto';
 import { RequestTicket, ResponseTicket } from './common/models/ticket.model';
 import { getTicketWithWinnings } from './common/utils/calculateWins';
 import { PowerballService } from './services/powerballService';
@@ -13,8 +14,14 @@ export class AppController {
 
   @Post()
   async getWinnings(@Body() body: RequestTicket): Promise<ResponseTicket> {
+    const { picks, date } = body;
+
+    const ticket: TicketDTO = new TicketDTO(date, picks);
+
+    const drawData = await this.powerballService.getDraw(date);
+    const draw: DrawDTO = new DrawDTO(drawData);
     
-    return null;
+    return getTicketWithWinnings(ticket.picks, draw);
   }
 
 }
